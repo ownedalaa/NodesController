9b965a9bb6783f3839ecb4e0c81b8e39ddf089d8
ownedalaa <il.scemo24@gmail.com>
2026-08-16 12:13:17 +0200

    cleaner implementation of the client's websocket handler

--- src/Agent/Program.cs
+++ src/Agent/Program.cs
@@ -1,54 +1,7 @@
-﻿using System.Net.WebSockets;
+﻿using Agent;
+using System.Net.WebSockets;
 using System.Text;
 
 string nodeId = "test-node-123";
 
-using var socket = new ClientWebSocket();
-
-
-await socket.ConnectAsync(
-    new Uri($"ws://localhost:5082/ws/agent/{nodeId}"),
-    CancellationToken.None
-);
-
-Console.WriteLine("Connected!");
-
-var buffer = new byte[4096];
-
-while (socket.State == WebSocketState.Open)
-{
-    var result = await socket.ReceiveAsync(
-        buffer,
-        CancellationToken.None
-    );
-
-    if (result.MessageType == WebSocketMessageType.Close)
-    {
-        Console.WriteLine("Server closed connection");
-        break;
-    }
-
-    var message = Encoding.UTF8.GetString(
-        buffer,
-        0,
-        result.Count
-    );
-
-    Console.WriteLine($"Server says: {message}");
-}
-
-SendAsync(socket, "Hello from client!").Wait();
-
-static async Task SendAsync(
-    ClientWebSocket socket,
-    string text)
-{
-    var bytes = Encoding.UTF8.GetBytes(text);
-
-    await socket.SendAsync(
-        bytes,
-        WebSocketMessageType.Text,
-        true,
-        CancellationToken.None
-    );
-}
+WebSocketHandler.HandleConnection(nodeId).Wait();
---
+++ src/Agent/WebSocketHandler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace Agent
+{
+    class WebSocketHandler
+    {
+        public static async Task HandleConnection(string nodeId)
+        {
+            using var socket = new ClientWebSocket();
+
+
+            await socket.ConnectAsync(
+                new Uri($"ws://localhost:5082/ws/agent/{nodeId}"),
+                CancellationToken.None
+            );
+
+            Console.WriteLine("Connected!");
+
+            var buffer = new byte[4096];
+
+
+            // send while conn is open
+            SendAsync(socket, "Hello from client!").Wait();
+
+
+
+            // receive
+            while (socket.State == WebSocketState.Open)
+            {
+                var result = await socket.ReceiveAsync(
+                    buffer,
+                    CancellationToken.None
+                );
+
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    Console.WriteLine("Server closed connection");
+                    break;
+                }
+
+                var message = Encoding.UTF8.GetString(
+                    buffer,
+                    0,
+                    result.Count
+                );
+
+                Console.WriteLine($"Server says: {message}");
+            }
+        }
+
+        private static async Task SendAsync(
+            ClientWebSocket socket,
+            string text)
+        {
+            var bytes = Encoding.UTF8.GetBytes(text);
+
+            await socket.SendAsync(
+                bytes,
+                WebSocketMessageType.Text,
+                true,
+                CancellationToken.None
+            );
+        }
+    }
+}
